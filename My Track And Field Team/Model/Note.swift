d547e9297f55ea1f87c320fb1dcdaae2ac0d9377
David Casillas <dcasillasappdev@gmail.com>
2020-03-28 11:59:54 -0400

    Athlete Note

---
+++ My Track And Field Team/Model/Note.swift
@@ -0,0 +1,47 @@
+//
+//  Note.swift
+//  My Track And Field Team
+//
+//  Created by David Casillas on 3/28/20.
+//  Copyright © 2020 Casillas Sports Apps. All rights reserved.
+//
+
+import Firebase
+
+class Note: NSObject {
+    
+    static let ID = "id"
+    static let COMMENT = "comment"
+    static let DATE_TIME = "dateTime"
+    
+    var id: String?
+    var comment: String?
+    var title: String?
+    var dateTime: Int?
+    
+    override init() {
+    }
+    
+    // from realtime database
+    init(snapshot: DataSnapshot) {
+        let dict = snapshot.value as! [String: Any]
+        self.id = dict[Note.ID] as? String ?? ""
+        self.comment = dict[Athlete.FIRST_NAME] as? String ?? ""
+        self.dateTime = dict[Note.DATE_TIME] as? Int ?? 0
+    }
+    
+    // you can create a Note object from the event result and it's belonging competition
+    init(eventResult: EventResult, competition: Competition) {
+        self.comment = eventResult.comment
+        self.dateTime = competition.dateTime
+        self.title = competition.name
+    }
+    
+    func toDict() -> [String: Any] {
+        var dict = [String: Any]()
+        dict[Note.ID] = id
+        dict[Note.COMMENT] = comment
+        dict[Note.DATE_TIME] = dateTime
+        return dict
+    }
+}
